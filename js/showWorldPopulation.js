3e920ad10bd46a498e08558b3cc5437678ff1a10
Stun4Life <weeweiren@gmail.com>
2021-01-11 16:28:40 +0800

    Updated Database

--- js/showWorldPopulation.js
+++ js/showWorldPopulation.js
@@ -3,32 +3,34 @@ $(document).ready(function () {
     var data = [];
     var labels = [];
 
-    var barChart = new Chart($("#barChart"), {
-        type: 'horizontalBar',
-        data: {
-            datasets: [{
-                    data: data,
-                    backgroundColor: "lightblue",
-                    label: 'Population'
-                }],
-            labels: labels
-        },
-        options: {
-            responsive: true
-        }
-    });
     $.ajax({
-                        type:"GET",
-                        url:"getStatistics.php",
-                        dataType: "JSON",
-                        success: function(response){
-                            for (i = 0; i < response.length; i++) {
-                                data.push(response[i].population);
-                                labels.push(response[i].country);
-                        }
-                    },
-                    error: function (obj, textStatus, errorThrown) {
-                        console.log("Error " + textStatus + ": " + errorThrown);
-                    },
-})
-});
+        type:"GET",
+        url:"getStatistics.php",
+        dataType: "JSON",
+        success: function(response){
+            for (i = 0; i < response.length; i++) {
+                data.push(response[i].population);
+                labels.push(response[i].country);
+            }
+        
+            var barChart = new Chart($("#barChart"), {
+                   type: 'horizontalBar',
+                   data: {
+                       datasets: [{
+                               data: data,
+                               backgroundColor: "lightblue",
+                               label: 'Population'
+                           }],
+                       labels: labels
+                   },
+                   options: {
+                       responsive: true
+                   }
+           });
+        },
+        error: function (obj, textStatus, errorThrown) {
+            console.log(obj);
+            console.log("Error " + textStatus + ": " + errorThrown);
+        },
+    })
+}); 
